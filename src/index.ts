a6612c9dd58b8270e27b48d0439968a061ecb0b8
Rian Rodrigues Gomes <rianrgomes@gmail.com>
2025-02-16 17:47:30 -0300

    feat: download video (I'll download video separated from audio because of ytdl docs limitations

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,41 @@
 import readline from 'readline';
+import ytdl from 'ytdl-core'
+import path from 'node:path'
+import fs from 'node:fs'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-const calculateDuration = (startTime: string, endTime: string) =>{
+const calculateDuration = (startTime: string, endTime: string): number =>{
   const [startHour, startMinute, startSecond] = startTime.split(':').map(Number)
   const [endHour, endMinute, endSecond] = endTime.split(':').map(Number)
 
   const duration = (endHour*3600 + endMinute*60 + endSecond) - (startHour*3600 + startMinute*60 + startSecond)
-  console.log(duration);
+  return duration
 }
 
 const processVideo = async (url: string, time1: string, time2: string) =>{
-  calculateDuration(time1, time2)
+  const duration: number = calculateDuration(time1, time2)
+  const videoPath = path.join(__dirname, 'assets', 'videos')
+  const audioPath = path.join(__dirname, 'assets', 'videos')
+
+  await new Promise<void>((resolve, reject) =>{
+    ytdl(url, {
+      filter: 'videoonly',
+      quality: '137'
+    }).pipe(fs.createWriteStream(path.join(videoPath, 'v1.mp4')))
+    .on('finish', () =>{
+      console.log('Vídeo baixado com sucesso!')
+    })
+  })
 }
 
 
